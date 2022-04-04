5ca599b3c9396bd1f4fe540bf4bb74a403a5085c
Ahmed Barazenji <ahmed1079227@gmail.com>
2022-04-04 12:59:02 +0300

    using ejs instead of normal html

--- app.js
+++ app.js
@@ -23,9 +23,14 @@ app.get('/', (req, res) => {
         res.render('index');
 });
 
-app.get('/main', (req, res) =>{
+app.get('/about', (req, res) =>{
     //res.send('<p>hello world!!!</p>');
-        res.sendFile('./views/about.html', {root: __dirname});
+        // res.sendFile('./views/about.html', {root: __dirname});
+        res.render('about');
+});
+
+app.get('/blogs/create', (req, res) =>{
+    res.render('create');
 });
 
 //redirect
@@ -35,5 +40,6 @@ app.get('/about-us', (req, res) =>{
 
 //error page
 app.use((req, res) =>{
-    res.sendFile('./views/404.html', {root: __dirname});
+    // res.sendFile('./views/404.html', {root: __dirname});
+    res.status(404).render('404')
 })
